007717b7380fd389bf69627a6018750ffbd10e57
Akshay Khole <akshay@reversehack.in>
2017-03-19 23:17:57 -0700

    checkpoint - car at least drives a little bit :)

--- model.py
+++ model.py
@@ -15,7 +15,6 @@ with open(base_path + "driving_log.csv") as csvfile:
 images = []
 measurements = []
 for line in lines[1:]:
-    print("Loading new line...")
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = base_path + "IMG/" + filename
@@ -29,13 +28,14 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3))) # normalizes all images
 model.add(Flatten(input_shape=(160, 320, 3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs = 8)
 
 model.save('model.h5')
