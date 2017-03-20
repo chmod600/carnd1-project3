f1a764afcd45989eb0027dd02745de0a18895567
Akshay Khole <akshay@reversehack.in>
2017-03-19 23:03:55 -0700

    checkpoint

--- model.py
+++ model.py
@@ -1,4 +1,9 @@
 import csv
+import numpy as np
+import cv2
+
+
+
 base_path = "/Users/Akshay/Desktop/p3/data/"
 lines = []
 with open(base_path + "driving_log.csv") as csvfile:
@@ -9,10 +14,28 @@ with open(base_path + "driving_log.csv") as csvfile:
 
 images = []
 measurements = []
-for line in lines:
+for line in lines[1:]:
+    print("Loading new line...")
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = base_path + "IMG/" + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
+    measurements.append(measurement)
+
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
