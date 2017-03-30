feb28823804951a80cc1097051882aa702b7f2e2
Akshay Khole <akshay@reversehack.in>
2017-03-30 00:44:18 -0700

    save

--- model.py
+++ model.py
@@ -83,7 +83,7 @@ from keras.layers import Dropout
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3))) # normalizes all images
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
 
 # model.add(Conv2D(24, (5, 5), strides = (2, 2), activation = "relu"))
 # model.add(Conv2D(36, (5, 5), strides = (2, 2), activation = "relu"))
@@ -93,7 +93,7 @@ model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation = "relu"))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation = "relu"))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation = "relu"))
 
-model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
 
 # model.add(Conv2D(64, (3, 3), strides = (2, 2), activation = "relu"))
 # model.add(Conv2D(64, (3, 3), strides = (2, 2), activation = "relu"))
