4a8b86b1d6f0a108115be830f5ded3f01b9456ec
Akshay Khole <akshay@reversehack.in>
2017-03-30 00:37:58 -0700

    revert to old version

--- model.py
+++ model.py
@@ -76,19 +76,31 @@ def generator(samples, batch_size = 32):
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Cropping2D
-from keras.layers import Conv2D
+# from keras.layers import Conv2D
+from keras.layers import Convolution2D
 from keras.layers import Dropout
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3))) # normalizes all images
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 model.add(Dropout(0.5))
-model.add(Conv2D(24, (5, 5), strides = (2, 2), activation = "relu"))
-model.add(Conv2D(36, (5, 5), strides = (2, 2), activation = "relu"))
-model.add(Conv2D(48, (5, 5), strides = (2, 2), activation = "relu"))
+
+# model.add(Conv2D(24, (5, 5), strides = (2, 2), activation = "relu"))
+# model.add(Conv2D(36, (5, 5), strides = (2, 2), activation = "relu"))
+# model.add(Conv2D(48, (5, 5), strides = (2, 2), activation = "relu"))
+
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation = "relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation = "relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation = "relu"))
+
 model.add(Dropout(0.5))
-model.add(Conv2D(64, (3, 3), strides = (2, 2), activation = "relu"))
-model.add(Conv2D(64, (3, 3), strides = (2, 2), activation = "relu"))
+
+# model.add(Conv2D(64, (3, 3), strides = (2, 2), activation = "relu"))
+# model.add(Conv2D(64, (3, 3), strides = (2, 2), activation = "relu"))
+
+model.add(Convolution2D(64, 3, 3, subsample=(2, 2), activation = "relu"))
+model.add(Convolution2D(64, 3, 3, subsample=(2, 2), activation = "relu"))
+
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
