cfaf589feb9f2e8d3fd5bbf519a04615b4ada47d
Akshay Khole <akshay@reversehack.in>
2017-04-02 01:41:17 -0700

    checkpoint

--- model.py
+++ model.py
@@ -135,6 +135,6 @@ model.compile(loss='mse', optimizer='adam')
 
 print("no of training samples - ", len(train_samples))
 # Start training the model, uses 3 Epochs since in our tests, beyond 3 epochs, losses started increasing
-model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data = validation_generator, nb_val_samples = len(validation_samples), nb_epoch = 3, verbose = 1)
+model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data = validation_generator, nb_val_samples = len(validation_samples), nb_epoch = 10, verbose = 1)
 # Save model
 model.save('model.h5')
